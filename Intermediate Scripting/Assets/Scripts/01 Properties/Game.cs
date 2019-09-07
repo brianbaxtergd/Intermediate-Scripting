4402e845ed6cb09848f5e23f6be1ffa928b19f8e
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-07 17:01:37 -0500

    Properties.

---
+++ Intermediate Scripting/Assets/Scripts/01 Properties/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player myPlayer = new Player();
+
+        myPlayer.Experience = 5;
+        int x = myPlayer.Experience;
+    }
+}
