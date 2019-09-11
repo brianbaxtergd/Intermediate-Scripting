dc7e4d5114ffdfaaa3c15a25f7c73752990ee59e
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-11 11:33:18 -0500

    Method overloading.

---
+++ Intermediate Scripting/Assets/Scripts/04 Method Overloading/SomeOtherClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeOtherClass : MonoBehaviour
+{
+    SomeClass inst = new SomeClass();
+
+    public void Start()
+    {
+        int num = inst.Add(1, 2);
+        string str = inst.Add("The", " one");
+    }
+}
