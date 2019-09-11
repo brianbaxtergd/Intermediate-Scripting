fda23d08eb0e43abf39130b8d782b7f4087b6aa3
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-11 12:04:01 -0500

    Inheritance.

---
+++ Intermediate Scripting/Assets/Scripts/06 Inheritance/Apple.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : Fruit
+{
+    public Apple()
+    {
+        color = "red";
+        Debug.Log("1st Apple Constructor Called.");
+    }
+
+    public Apple(string newColor) : base(newColor)
+    {
+        Debug.Log("2nd Apple Constructor Called.");
+    }
+}
---
+++ Intermediate Scripting/Assets/Scripts/06 Inheritance/FruitSalad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSalad : MonoBehaviour
+{
+    void Start()
+    {
+        // Illustrate inheritance with default constructors.
+        Debug.Log("Creating the fruit...");
+        Fruit myFruit = new Fruit();
+        Debug.Log("Creating the apple...");
+        Apple myApple = new Apple();
+
+        // Call methods of the Fruit class.
+        myFruit.SayHello();
+        myFruit.Chop();
+
+        // Call methods of the Apple class.
+        // Apple has access to all of the public thods of class Fruit.
+        myApple.SayHello();
+        myApple.Chop();
+
+        // Illustrate inheritance with the contructors that read in a string.
+        Debug.Log("Creating the fruit...");
+        myFruit = new Fruit("yellow");
+        Debug.Log("Creating the apple...");
+        myApple = new Apple("green");
+
+        // Call methods of the Fruit class.
+        myFruit.SayHello();
+        myFruit.Chop();
+
+        // Call methods of the Apple class.
+        // Apple has access to all of the public methods of class Fruit.
+        myApple.SayHello();
+        myApple.Chop();
+    }
+}
