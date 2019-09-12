2f2315cfe43515643b49f34b709ce6f04139cd13
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-12 12:56:28 -0500

    Delegates.

---
+++ Intermediate Scripting/Assets/Scripts/16 Delegates/DelegateScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateScript : MonoBehaviour
+{
+    delegate void MyDelegate(int num);
+    MyDelegate myDelegate;
+
+
+    private void Start()
+    {
+        myDelegate = PrintNum;
+        myDelegate(50);
+
+        myDelegate = DoubleNum;
+        myDelegate(50);
+    }
+
+    void PrintNum(int num)
+    {
+        print("Print num: " + num);
+    }
+
+    void DoubleNum(int num)
+    {
+        print("Double num: " + num * 2);
+    }
+}
---
+++ Intermediate Scripting/Assets/Scripts/16 Delegates/MulticastScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulticastScript : MonoBehaviour
+{
+    delegate void MultiDelegate();
+    MultiDelegate myMultiDelegate;
+
+
+    private void Start()
+    {
+        myMultiDelegate += PowerUp;
+        myMultiDelegate += TurnRed;
+
+        if (myMultiDelegate != null)
+            myMultiDelegate();
+    }
+
+    void PowerUp()
+    {
+        print("Orb is powering up!");
+    }
+
+    void TurnRed()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
