9631dab338c5fbb3f558bad90e676467ecfb420e
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-12 11:56:35 -0500

    Lists and dictionaries.

---
+++ Intermediate Scripting/Assets/Scripts/13 Lists and Dictionaries/SomeClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeClass : MonoBehaviour
+{
+    void Start()
+    {
+        List<BadGuy> badguys = new List<BadGuy>();
+
+        badguys.Add(new BadGuy("Harvey", 50));
+        badguys.Add(new BadGuy("Magneto", 100));
+        badguys.Add(new BadGuy("Pip", 5));
+
+        badguys.Sort();
+        foreach(BadGuy guy in badguys)
+        {
+            print(guy.name + " " + guy.power);
+        }
+
+        badguys.Clear();
+    }
+}
