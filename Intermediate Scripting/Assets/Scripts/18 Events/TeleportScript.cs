9b95ff6bc37cac22d8ea8a8833d6d5158cb8afa4
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-12 13:19:02 -0500

    Events.

---
+++ Intermediate Scripting/Assets/Scripts/18 Events/TeleportScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        EventManager.OnClicked += Teleport;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnClicked -= Teleport;
+    }
+
+    void Teleport()
+    {
+        Vector3 pos = transform.position;
+        pos.y = Random.Range(0.3f, 1.0f);
+        transform.position = pos;
+    }
+}
---
+++ Intermediate Scripting/Assets/Scripts/18 Events/TurnColorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnColorScript : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        EventManager.OnClicked += TurnColor;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnClicked -= TurnColor;
+    }
+
+    void TurnColor()
+    {
+        Color col = new Color(Random.value, Random.value, Random.value);
+        GetComponent<Renderer>().material.color = col;
+    }
+}
